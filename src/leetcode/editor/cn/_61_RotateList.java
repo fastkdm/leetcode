56f4e7fbe4945354a31349be15ede9be0f872a74
liucy <cyliu1996@foxmail.com>
2021-09-25 23:05:30 +0800

    RotateList

---
+++ src/leetcode/editor/cn/_61_RotateList.java
@@ -0,0 +1,90 @@
+//给你一个链表的头节点 head ，旋转链表，将链表每个节点向右移动 k 个位置。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：head = [1,2,3,4,5], k = 2
+//输出：[4,5,1,2,3]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = [0,1,2], k = 4
+//输出：[2,0,1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点的数目在范围 [0, 500] 内 
+// -100 <= Node.val <= 100 
+// 0 <= k <= 2 * 10⁹ 
+// 
+// 👍 634 👎 0
+
+package leetcode.editor.cn;
+
+import java.util.List;
+
+public class _61_RotateList {
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode rotateRight(ListNode head, int k) {
+            if(head == null){
+                return null;
+            }
+            ListNode _head = head;
+            ListNode _tail = head;
+            int size = 0;
+            while (_head != null){
+                _tail = _head;
+                _head = _head.next;
+                size++;
+            }
+            _tail.next = head;
+            int offset = size - k%size;
+            for (int i = 0;i<offset;i++){
+                _tail = _tail.next;
+            }
+            final ListNode next = _tail.next;
+            _tail.next = null;
+            return next;
+        }
+
+        public class ListNode {
+            int val;
+            ListNode next;
+
+            ListNode() {
+            }
+
+            ListNode(int val) {
+                this.val = val;
+            }
+
+            ListNode(int val, ListNode next) {
+                this.val = val;
+                this.next = next;
+            }
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
